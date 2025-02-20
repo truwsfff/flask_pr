deaa482e16524436d2c1e25b7898dc67bd428eaf
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-02-20 16:05:43 +0300

    task2

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def welcome_m():
     return "Миссия Колонизация Марса"
 
 @app.route('/index')
-def countdown():
+def text():
     return 'И на Марсе будут яблони цвести!'
+
+@app.route('/promotion')
+def ad():
+    sp = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+          'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+          'Присоединяйся!']
+    return '</br></br>'.join(sp)
 
 
 if __name__ == '__main__':
