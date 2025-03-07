8364468f6fde6892c544f4c3b903a2752c6a3d53
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-03-08 01:48:31 +0300

    task4.modified

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome_m():
     return "Миссия Колонизация Марса"
+
 
 @app.route('/index')
 def text():
     return 'И на Марсе будут яблони цвести!'
 
+
 @app.route('/promotion')
 def ad():
-    sp = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-          'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
+    sp = ['Человечество вырастает из детства.',
+          'Человечеству мала одна планета.',
+          'Мы сделаем обитаемыми безжизненные пока планеты.',
+          'И начнем с Марса!',
           'Присоединяйся!']
     return '</br></br>'.join(sp)
+
 
 @app.route('/image_mars')
 def image():
@@ -32,13 +38,16 @@ def image():
                       </body>
                     </html>"""
 
+
 @app.route('/promotion_image')
 def ad_image():
     return """<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
-                            <link rel="stylesheet" type="text/css" href="static/css/style.css" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                            
                             <title>Привет, Марс!</title>
                           </head>
                           <body>
@@ -48,15 +57,19 @@ def ad_image():
                             <div class="alert alert-dark" role="alert">
   Человечество вырастает из детства.
 </div>
+
                             <div class="alert alert-success" role="alert">
   Человечеству мала одна планета.
-</div
+</div>
+
                             <div class="alert alert-secondary" role="alert">
   Мы сделаем обитаемыми безжизненные пока планеты.
-</div
+</div>
+
                             <div class="alert alert-warning" role="alert">
   И начнем с Марса
 </div>
+
                             <div class="alert alert-danger" role="alert">
   Присоединяйся!
 </div>
