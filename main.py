56ae0d157c928440833c4c81e5c7d1e5284281bc
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-03-08 03:39:57 +0300

    1-hometask.modified

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def ad_image(planet_name):
                             <meta name="viewport" content="width=device-width, initial-scale=1">
                             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                             
-                            <title>Привет, Марс!</title>
+                            <title>Варианты выбора</title>
                           </head>
                           <body>
                             <h1>Мое предложение: {planet_name}</h1>
