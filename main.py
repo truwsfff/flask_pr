898691a7c7c74a76af91dc8eb0b5fe21210ade44
truwsfff <138609244+truwsfff@users.noreply.github.com>
2025-03-08 03:51:37 +0300

    2-hometask

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet_name>')
-def ad_image(planet_name):
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def ad_image(nickname, level, rating):
     return f"""<!doctype html>
                         <html lang="en">
                           <head>
@@ -12,26 +12,22 @@ def ad_image(planet_name):
                             <meta name="viewport" content="width=device-width, initial-scale=1">
                             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                             
-                            <title>Варианты выбора</title>
+                            <title>Результаты</title>
                           </head>
                           <body>
-                            <h1>Мое предложение: {planet_name}</h1>
-                            <h4>Эта планета близка к Земле;</h4>
+                            <h1>Результаты отбора</h1>
+                            <h3>Претендента на участие в миссии {nickname}:</h3>
 
                             <div class="alert alert-success" role="alert">
-  <h4>На ней много необходимых ресурсов;</h4>
+  <h4>Поздравляем! Ваш рейтинг после {level} этапа отбора</h4>
 </div>
 
-                            <div class="alert alert-secondary" role="alert">
-  <h4>На ней есть вода и атмосфера;</h4>
+                            <div class="alert alert-light" role="alert">
+  <h4>составляет {rating}!</h4>
 </div>
 
                             <div class="alert alert-warning" role="alert">
-  <h4>На ней есть небольшое магнитное поле;</h4>
-</div>
-
-                            <div class="alert alert-danger" role="alert">
-  <h4>Наконец, она просто красива!</h4>
+  <h3>Желаем удачи!</h3>
 </div>
                           </body>
                         </html>"""
